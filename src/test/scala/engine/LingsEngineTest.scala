b3ebeb7f776c1731ac7729a02e3497f80efc393e
Nick Pierson <nkcrpn@gmail.com>
2017-02-22 19:36:21 -0600

    test LingsEngine turn event

--- src/test/scala/engine/LingsEngineTest.scala
+++ src/test/scala/engine/LingsEngineTest.scala
@@ -2,18 +2,23 @@ package engine
 
 import _root_.agent.LingsAgent
 import agent.LingsAgent.{AgentState, EmptyState}
-import client.LingsProtocol.InMessage
+import client.LingsProtocol.{InMessage, OutMessage}
+import engine.LingsEngine.SendMessage
 import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
-import org.scalatest.{BeforeAndAfterEach, WordSpec}
+import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
 
-class LingsEngineTest extends WordSpec with BeforeAndAfterEach with MockitoSugar {
+class LingsEngineTest extends WordSpec with Matchers with BeforeAndAfterEach with MockitoSugar {
 
-  val message: InMessage = mock[InMessage]
+  val inMessage: InMessage = mock[InMessage]
   val agent: LingsAgent = mock[LingsAgent]
 
-  var mockPerceive: Function[InMessage, AgentState => AgentState]  = _
-  var mockPerceived: Function[AgentState, AgentState] = _
+  var mockPerceive:  InMessage => AgentState => AgentState = _
+  var mockPerceived: AgentState => AgentState = _
+
+  var nextAction: AgentState => Option[OutMessage] = _
+  var send:       SendMessage = _
+  var outMessage: OutMessage = _
 
   var lingsEngine: LingsEngine = _
 
@@ -23,20 +28,24 @@ class LingsEngineTest extends WordSpec with BeforeAndAfterEach with MockitoSugar
 
     lingsEngine = LingsEngine(agent)
 
+    send = mock[SendMessage]
+    outMessage = mock[OutMessage]
+    nextAction = mock[AgentState => Option[OutMessage]]
+    when(agent.nextAction).thenReturn(nextAction)
+    when(nextAction.apply(EmptyState)).thenReturn(Some(outMessage))
     when(agent.perceive).thenReturn(mockPerceive)
-    when(mockPerceive.apply(message)).thenReturn(mockPerceived)
+    when(mockPerceive.apply(inMessage)).thenReturn(mockPerceived)
   }
 
   "Lings Engine" should {
-
     "forward perceptions to the agent" in {
-      lingsEngine.perceive(message)
+      lingsEngine.perceive(inMessage)
 
-      verify(mockPerceive).apply(message)
+      verify(mockPerceive).apply(inMessage)
     }
 
     "start with empty state" in {
-      lingsEngine.perceive(message)
+      lingsEngine.perceive(inMessage)
 
       verify(mockPerceived).apply(EmptyState)
     }
@@ -45,13 +54,26 @@ class LingsEngineTest extends WordSpec with BeforeAndAfterEach with MockitoSugar
       val expectedState = mock[AgentState]
       when(mockPerceived.apply(EmptyState)).thenReturn(expectedState)
 
-      lingsEngine.perceive(message)
+      lingsEngine.perceive(inMessage)
 
       verify(mockPerceived).apply(EmptyState)
 
-      lingsEngine.perceive(message)
+      lingsEngine.perceive(inMessage)
 
       verify(mockPerceived).apply(expectedState)
+    }
+
+    "take 15 ticks per turn" in {
+      val ticks = lingsEngine.onTurn(send)
+
+      ticks           shouldBe defined
+      ticks.get.value shouldBe 15
+    }
+
+    "send the agent action during a turn" in {
+      lingsEngine.onTurn(send)
+
+      verify(send).apply(outMessage)
     }
   }
 }
