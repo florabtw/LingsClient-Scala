ef3ce571c46b49cb6ac57dcb35ab87f3783f6001
Nick Pierson <nkcrpn@gmail.com>
2017-02-27 21:29:19 -0600

    abstract entity messages into single type

--- src/main/scala/client/Parser.scala
+++ src/main/scala/client/Parser.scala
@@ -2,19 +2,26 @@ package client
 
 import akka.http.scaladsl.model.ws.TextMessage
 import client.LingsProtocol._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 
 object Parser {
   def toLingsMessage(msgString: String): InMessage = {
     val msgJson = Json.parse(msgString)
     val msgType = (msgJson \ "type").as[String]
     msgType match {
-      case "map"   => msgJson.as[MapMessage]
-      case "agent" => msgJson.as[AgentMessage]
+      case "map"    => msgJson.as[MapMessage]
+      case "entity" => parseEntity(msgJson)
+      case "id"     => msgJson.as[IdMessage]
+      case "move"   => msgJson.as[AgentMoveMessage]
+      case "eat"    => msgJson.as[AgentEatMessage]
+    }
+  }
+
+  private def parseEntity(msgJson: JsValue): InMessage = {
+    val entityType = (msgJson \ "entityType").as[String]
+    entityType match {
       case "food"  => msgJson.as[FoodMessage]
-      case "id"    => msgJson.as[IdMessage]
-      case "move"  => msgJson.as[AgentMoveMessage]
-      case "eat"   => msgJson.as[AgentEatMessage]
+      case "agent" => msgJson.as[AgentMessage]
     }
   }
 
